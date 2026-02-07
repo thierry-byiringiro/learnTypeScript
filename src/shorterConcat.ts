9c2a03a035addb013ca8c67507dc8ea6791fdba2
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-07 19:03:21 +0200

    work with strings lengh

--- src/shorterConcat.ts
+++ src/shorterConcat.ts
@@ -1,4 +1,15 @@
- function shorterReverseLonger(a: string, b: string): string {
-    
-    return ""
+function shorterReverseLonger(a: string, b: string): string {
+    if (a.length > b.length) {
+        a = a.split("").reverse().join("");
+        return b.concat(a).concat(b);
+        return "";
+    } else if (b.length > a.length) {
+        b = b.split("").reverse().join();
+        return a.concat(b).concat(a);
+    } else {
+        a = a.split("").reverse().join("");
+        return b.concat(a).concat(b);
+
+    }
 }
+console.log(shorterReverseLonger("first", "abcde"));//abcdetsrifabcde
